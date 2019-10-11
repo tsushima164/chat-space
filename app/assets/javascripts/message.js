24542080cdb49d3b2347f47f699932b21c87ae22
Tsushima164 <tsushimahiroshi60@gmail.com>
2019-10-11 12:29:50 +0900

    自動更新修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -46,61 +46,6 @@ $(function() {
     });
   });
 
-  var buildMessageHTML = function(message) {
-    if (message.content && message.image) {
-      //data-idが反映されるようにしている
-      var html = `<div class="message" data-id="${message.id}">'
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.time}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${message.content}
-                      </p>
-                      <img src="${message.image}" class="lower-message__image" >
-                    </div>
-                  </div>`
-    } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-id=${message.id}>
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.time}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${message.content}
-                      </p>
-                    </div>
-                  </div>`
-    } else if (message.image) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-id=${message.id}">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.time}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <img src="${message.image}" class="lower-message__image" >
-                    </div>
-                  </div>`
-    };
-    return html;
-  };
-
   var reloadMessages = function() {
     var last_message_id = $('.message:last').data("id");
     $.ajax({
